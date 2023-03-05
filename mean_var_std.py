8c5d0a907dbf6ed71ed9ffba1c96119c713eb0bd
Anna <39511761+annatakegawa@users.noreply.github.com>
2023-03-05 06:48:18 +0000

    Initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,29 @@
 import numpy as np
 
+
 def calculate(list):
 
+  if len(list) == 9:
+    matrix = np.array(list).reshape(3, 3)
 
+    calculations = {
+      'mean': np.mean,
+      'variance': np.var,
+      'standard deviation': np.std,
+      'max': np.max,
+      'min': np.min,
+      'sum': np.sum
+    }
 
+    for key, value in calculations.items():
+      op = value
+      calculations[key] = [
+        op(matrix, axis=0).tolist(),
+        op(matrix, axis=1).tolist(),
+        op(matrix)
+      ]
 
     return calculations
+
+  else:
+    raise ValueError('List must contain nine numbers.')
